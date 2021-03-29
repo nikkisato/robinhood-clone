50f526bd9480eac0546af2dc201922caa4688840
Nikki <nikki.satopdx@gmail.com>
2021-03-29 13:24:13 -0700

    Created linegraph

--- src/Components/Newsfeed/Newsfeed.js
+++ src/Components/Newsfeed/Newsfeed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Newsfeed.css';
+import LineGraph from '../LineGraph/LineGraph';
 
 function Newsfeed() {
   return (
@@ -9,6 +10,9 @@ function Newsfeed() {
           <div className='newsfeed__portfolio'>
             <h1> $114,656,84</h1>
             <p> $142.90 (-0,12) Today </p>
+          </div>
+          <div className='newsfeed__chart'>
+            <LineGraph />
           </div>
         </div>
       </div>
