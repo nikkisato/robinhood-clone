c11a5a289dba81eb9d4dad3f3c530367770e0c9e
Nikki <nikki.satopdx@gmail.com>
2021-03-29 15:15:08 -0700

    got the stats

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -1,7 +1,117 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Stats.css';
+import axios from 'axios';
+import { db } from '../../firebase';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import StatsRow from '../StatsRow/StatsRow';
+import { key } from '../../api';
+
+const BASE_URL = 'https://finnhub.io/api/v1/quote?symbol=';
+const KEY_URL = `&token=${key}`;
+console.log(KEY_URL);
+
+const testData = [];
+
 function Stats() {
-  return <div></div>;
+  const [stocksData, setStocksData] = useState([]);
+  const [myStocks, setMyStocks] = useState([]);
+
+  const getMyStocks = () => {
+    db.collection('myStocks').onSnapshot(snapshot => {
+      let promises = [];
+      let tempData = [];
+      snapshot.docs.map(doc => {
+        return promises.push(
+          getStocksData(doc.data().ticker).then(res => {
+            tempData.push({
+              id: doc.id,
+              data: doc.data(),
+              info: res.data,
+            });
+          })
+        );
+      });
+      Promise.all(promises).then(() => {
+        setMyStocks(tempData);
+      });
+    });
+  };
+
+  const getStocksData = stock => {
+    return axios.get(`${BASE_URL}${stock}${KEY_URL}`).catch(error => {
+      console.log('ERROR', error.message);
+    });
+  };
+
+  useEffect(() => {
+    const stocksList = [
+      'AAPL',
+      'MSFT',
+      'TSLA',
+      'FB',
+      'BABA',
+      'UBER',
+      'DIS',
+      'SBUX',
+    ];
+
+    getMyStocks();
+    let promises = [];
+    stocksList.map(stock => {
+      return promises.push(
+        getStocksData(stock).then(res => {
+          testData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      console.log(testData);
+      setStocksData(testData);
+    });
+  }, []);
+
+  return (
+    <div className='stats'>
+      <div className='stats__container'>
+        <div className='stats__header'>
+          <p> Stocks</p>
+          <MoreHorizIcon />
+        </div>
+        <div className='stats__content'>
+          <div className='stats__rows'>
+            {myStocks.map(stock => (
+              <StatsRow
+                key={stock.data.ticker}
+                name={stock.data.ticker}
+                openPrice={stock.info.o}
+                volume={stock.data.shares}
+                price={stock.info.c}
+              />
+            ))}
+          </div>
+        </div>
+        <div className='stats__header stats-lists'>
+          <p>Lists</p>
+        </div>
+        <div className='stats__content'>
+          <div className='stats__rows'>
+            {stocksData.map(stock => (
+              <StatsRow
+                key={stock.name}
+                name={stock.name}
+                openPrice={stock.o}
+                price={stock.c}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
